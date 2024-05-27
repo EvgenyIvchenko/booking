a5f6f8042bb3cbb1deea99f837de6a6344260c07
Evgeny <dirath2014@gmail.com>
2024-05-27 17:13:07 +0300

    Добавит слайдер полю цены за ночь

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {createAnnouncements} from './modules/data.js';
 import {validateForm} from './modules/user-form.js';
 import {disableState} from './modules/state.js';
 import {initMap} from './modules/map.js';
+import {slider} from './modules/slider.js';
 
 const ANNOUNCEMENTS_COUNT = 10;
 const announcements = createAnnouncements(ANNOUNCEMENTS_COUNT);
@@ -9,3 +10,4 @@ const announcements = createAnnouncements(ANNOUNCEMENTS_COUNT);
 validateForm();
 disableState();
 initMap(announcements);
+slider();
---
+++ js/modules/slider.js
@@ -0,0 +1,20 @@
+const sliderElement = document.querySelector('.ad-form__slider');
+const valueElement = document.querySelector('#price');
+
+
+export const slider = () => {
+  noUiSlider.create(sliderElement, {
+    range: {
+      min: 0,
+      max: 100000,
+    },
+    start: 50000,
+    step: 1,
+    connect: 'lower',
+  });
+
+  sliderElement.noUiSlider.on('update', () => {
+    valueElement.value = sliderElement.noUiSlider.get();
+  });
+};
+
